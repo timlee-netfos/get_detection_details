ac1cffcf34016023328e3c12cf6925ca7777739d
timlee-netfos <tim.lee@netfos.com.tw>
2023-03-08 13:55:00 +0800

    update

--- key_assets.py
+++ key_assets.py
@@ -19,11 +19,14 @@ payload = {
 
 key_assets_ip = []
 
-# r = ExtraHop_API.patch_info("devices/60129551390", payload=payload)
-# r1 = ExtraHop_API.get_info("devices/60129551390")
-# print(r1.json())
-r2 = ExtraHop_API.get_info("devices")
+print("working on ---> get key assets id")
+r1 = ExtraHop_API.get_info("devices")
 with open(f"data/{ExtraHop_API.customer}_devices.json", "w") as fw:
-    json.dump(r2.json(), fw, indent=4)
-key_assets_id = [r["id"] for r in r2.json() if r["ipaddr4"] in key_assets_ip]
+    json.dump(r1.json(), fw, indent=4)
+key_assets_id = [r["id"] for r in r1.json() if r["ipaddr4"] in key_assets_ip]
+
+for id in key_assets_id:
+    print("working on ---> PATCHing key assets")
+    ExtraHop_API.patch_info(f"devices/{id}", payload=payload)
+
 print(colored("DONE!", "green"))
